2b4d614376e6b9d51386826ebb5c44c4d3dc7826
Mr-Williams11 <joelwilliams742@gmail.com>
2024-08-20 14:35:17 +0200

    calculator fix2

--- src/views/CalculatorView.vue
+++ src/views/CalculatorView.vue
@@ -1,39 +1,40 @@
 <template>
-    <div class="home">
-      <img
-        alt="background"
-        :class="{ fadeIn: isImageLoaded }"
-        src="../assets/AdobeStock_519445978-scaled.jpeg.webp"
-        @load="startAnimation"
-      />
-    </div>
+  <div class="home">
+    <img
+      alt="background"
+      :class="{ fadeIn: isImageLoaded }"
+      src="../assets/AdobeStock_519445978-scaled.jpeg.webp"
+      @load="startAnimation"
+    />
     <div id="app">
       <div class="container">
         <h1>Energy Calculator</h1>
-        <div class="form-group">
-          <label for="day">Select Day:</label>
-          <input type="number" id="day" v-model.number="day" />
-        </div>
-        <div class="form-group">
-          <label for="rate">Price Rate (R per kWh):</label>
-          <input
-            type="number"
-            id="rate"
-            v-model.number="rate"
-            step="0.01"
-            placeholder="Enter price rate"
-          />
-        </div>
-        <div class="form-group">
-          <label for="capacity">Energy Generated (kWh):</label>
-          <input
-            type="number"
-            id="capacity"
-            v-model.number="capacity"
-            step="0.01"
-            placeholder="Enter energy generated"
-            readonly
-          />
+        <div class="form-group-container">
+          <div class="form-group">
+            <label for="day">Select Day:</label>
+            <input type="number" id="day" v-model.number="day" />
+          </div>
+          <div class="form-group">
+            <label for="rate">Price Rate (R per kWh):</label>
+            <input
+              type="number"
+              id="rate"
+              v-model.number="rate"
+              step="0.01"
+              placeholder="Enter price rate"
+            />
+          </div>
+          <div class="form-group">
+            <label for="capacity">Energy Generated (kWh):</label>
+            <input
+              type="number"
+              id="capacity"
+              v-model.number="capacity"
+              step="0.01"
+              placeholder="Enter energy generated"
+              readonly
+            />
+          </div>
         </div>
         <div class="form-group">
           <label for="energyUsed">Energy Used (kWh):</label>
@@ -52,7 +53,7 @@
         <div class="result">{{ resultMessage }}</div>
         <canvas id="energyChart" width="400" height="200"></canvas>
       </div>
-  
+
       <div id="register">
         <div id="ticket">
           <h1>Your Receipt</h1>
@@ -75,187 +76,188 @@
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import { mapActions, mapState } from "vuex";
-  import Chart from "chart.js/auto";
-  
-  export default {
-    data() {
-      return {
-        day: null,
-        rate: 1, // Initialize with a default value
-        resultMessage: "",
-      };
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+import Chart from "chart.js/auto";
+
+export default {
+  data() {
+    return {
+      day: null,
+      rate: 1, // Initialize with a default value
+      resultMessage: "",
+    };
+  },
+  computed: {
+    ...mapState(["capacity", "energyUsed", "totalMoneyEarned"]),
+    savedUnits() {
+      return this.capacity - this.energyUsed;
     },
-    computed: {
-      ...mapState(["capacity", "energyUsed", "totalMoneyEarned"]),
-      savedUnits() {
-        return this.capacity - this.energyUsed;
-      },
-      resultMessage() {
-        if (this.capacity !== null && this.energyUsed !== null) {
-          return `Energy Generated - ${this.capacity} kWh, Energy Used - ${this.energyUsed} kWh`;
-        }
-        return "";
-      },
+    resultMessage() {
+      if (this.capacity !== null && this.energyUsed !== null) {
+        return `Energy Generated - ${this.capacity} kWh, Energy Used - ${this.energyUsed} kWh`;
+      }
+      return "";
     },
-    methods: {
-      ...mapActions(["fetchData", "calculateReturn"]),
-      async fetchData() {
-        if (!this.day) {
-          this.resultMessage = "Please select a day.";
-          return;
-        }
-  
-        try {
-          await this.$store.dispatch("fetchData", this.day);
-          // Update the rate in the Vuex store
-          this.$store.commit("SET_RATE", this.rate);
-          this.resultMessage = `Data fetched: Energy Generated: ${this.capacity} kWh, Energy Used: ${this.energyUsed} kWh`;
-          this.calculateReturn();
-          this.updateChart();
-        } catch (error) {
-          this.resultMessage = `Error fetching data: ${error.message}`;
-        }
-      },
-      calculateReturn() {
-        this.$store.dispatch("calculateReturn");
-      },
-      updateChart() {
-        const ctx = document.getElementById("energyChart").getContext("2d");
-        if (this.chart) {
-          this.chart.destroy();
-        }
-        this.chart = new Chart(ctx, {
-          type: "bar",
-          data: {
-            labels: ["Energy Generated", "Energy Used", "Excess Energy"],
-            datasets: [
-              {
-                label: "Energy (kWh)",
-                data: [this.capacity, this.energyUsed, this.savedUnits],
-                backgroundColor: ["#ff9a00", "#ffcc00", "#66cc66"],
-                borderColor: ["#e87c00", "#ffd700", "#44aa44"],
-                borderWidth: 1,
-              },
-            ],
-          },
-          options: {
-            responsive: true,
-            scales: {
-              x: {
-                beginAtZero: true,
-              },
-              y: {
-                beginAtZero: true,
-              },
+  },
+  methods: {
+    ...mapActions(["fetchData", "calculateReturn"]),
+    async fetchData() {
+      if (!this.day) {
+        this.resultMessage = "Please select a day.";
+        return;
+      }
+
+      try {
+        await this.$store.dispatch("fetchData", this.day);
+        // Update the rate in the Vuex store
+        this.$store.commit("SET_RATE", this.rate);
+        this.resultMessage = `Data fetched: Energy Generated: ${this.capacity} kWh, Energy Used: ${this.energyUsed} kWh`;
+        this.calculateReturn();
+        this.updateChart();
+      } catch (error) {
+        this.resultMessage = `Error fetching data: ${error.message}`;
+      }
+    },
+    calculateReturn() {
+      this.$store.dispatch("calculateReturn");
+    },
+    updateChart() {
+      const ctx = document.getElementById("energyChart").getContext("2d");
+      if (this.chart) {
+        this.chart.destroy();
+      }
+      this.chart = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: ["Energy Generated", "Energy Used", "Excess Energy"],
+          datasets: [
+            {
+              label: "Energy (kWh)",
+              data: [this.capacity, this.energyUsed, this.savedUnits],
+              backgroundColor: ["#FFCC00", "#FF9900", "#99CC00"],
+              borderColor: ["#FFCC00", "#FF9900", "#99CC00"],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              beginAtZero: true,
+            },
+            y: {
+              beginAtZero: true,
             },
           },
-        });
-      },
+        },
+      });
     },
-    watch: {
-      capacity() {
-        this.updateChart();
-      },
-      energyUsed() {
-        this.updateChart();
-      },
+  },
+  watch: {
+    capacity() {
+      this.updateChart();
     },
-  };
-  </script>
+    energyUsed() {
+      this.updateChart();
+    },
+  },
+};
+</script>
 
 <style scoped>
 .home {
-  position: absolute; /* Changed to absolute positioning */
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  z-index: -1; /* Ensure it’s behind other content */
-  overflow: hidden;
+  position: relative;
+  color: white;
 }
 
-.home img {
+img {
   filter: grayscale(1);
-  height: 100%;
+  transition: filter 2s ease-in;
+  height: 100vh;
   width: 100%;
   object-fit: cover;
+  position: absolute; /* Ensure the image is positioned absolutely */
+  top: 0;
+  left: 0;
+  z-index: 1; /* Send image behind the content */
 }
 
-body {
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-  margin: 0;
+img.fadeIn {
+  filter: grayscale(0);
 }
 
 #app {
-  position: relative;
-  top: 20px; 
-  z-index: 1; 
+  position: relative; /* Position relative to allow stacking on top of image */
+  z-index: 2; /* Ensure the content is on top of the image */
   width: 100%;
   display: flex;
   align-items: center;
+  padding: 20px;
+  box-sizing: border-box;
   justify-content: center;
 }
 
 .container {
-  background-color: white;
-  position: relative;
-  top: 20px;
+  justify-content: center;
+  background: #FFFFFF;
   padding: 20px;
   border-radius: 12px;
-  box-shadow: 0 4px 8px black;
-  max-width: 500px;
-  width: 100%;
-  margin: 20px;
-  height: auto;
-  border: 1px solid black;
-  z-index: 2;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.2);
+  width: auto;
+  max-width: 600px;
+  box-sizing: border-box;
+  margin: 10px;
 }
 
 h1 {
-  color: #023b21;
+  color: #FF9900;
   margin-bottom: 20px;
-  font-size: 22px;
+  font-size: 20px;
   font-weight: 600;
-  background: linear-gradient(
-    90deg,
-    #00f3f0,
-    #053bfd
-  ); /* Gradient for heading */
+  background: linear-gradient(90deg, #FFCC00, #FF9900);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }
 
+.form-group-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+
 .form-group {
-  display: inline-block;
-  width: 30%;
-  margin-right: 10px;
+  flex: 1;
+  min-width: calc(33.333% - 10px);
+  box-sizing: border-box;
+  margin-bottom: 15px;
 }
 
 .form-group label {
-  font-size: 10px;
-  font-weight: bold;
+  font-size: 15px;
+  display: block;
+  margin-bottom: 6px;
+  font-weight: 500;
+  color: #FF9900;
 }
 
 .form-group input {
   width: 100%;
-  padding: 6px;
-  font-size: 12px;
+  padding: 8px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
   box-sizing: border-box;
+  font-size: 14px;
 }
 
 .form-group input:focus {
-  border-color: #00ff89; /* Light green for focus border */
+  border-color: #FFCC00;
   outline: none;
-  box-shadow: 0 0 5px rgba(0, 255, 137, 0.5);
+  box-shadow: 0 0 5px rgba(255, 204, 0, 0.5);
 }
 
 .button-group {
@@ -265,7 +267,7 @@ h1 {
 }
 
 button {
-  background-color: #023b21; /* Dark green for buttons */
+  background-color: #FF9900;
   color: white;
   border: none;
   padding: 10px 16px;
@@ -276,56 +278,57 @@ button {
 }
 
 button:hover {
-  background-color: #00ff89; /* Light green for hover */
+  background-color: #FFCC00;
 }
 
 .result {
   margin-top: 15px;
   font-size: 16px;
   font-weight: 600;
-  color: #00ff89; /* Light green for success messages */
+  color: #FFCC00;
   text-align: center;
 }
 
 #error {
-  color: #023b21; /* Dark green for errors */
+  color: #FF9900;
   font-size: 16px;
   font-weight: 500;
 }
 
 .success {
-  color: #00ff89; /* Light green for success messages */
+  color: #FFCC00;
   font-size: 16px;
   font-weight: 500;
 }
 
 #register {
   width: 100%;
-  max-width: 500px;
+  max-width: 600px;
 }
 
 #ticket {
+  background: #FFFFFF;
+  color: black;
   margin: auto;
   padding: 1em;
   border-radius: 12px;
-  box-shadow: 0 4px 8px black;
-  border: 1px solid black;
-  background-color: white;
+  box-shadow: 0 0 5px rgba(0,0,0,.25);
+  width: 100%;
+  max-width: 500px;
 }
 
 #ticket h1 {
   text-align: center;
-  color: #023b21; /* Dark green for heading */
+  color: #FF9900;
 }
 
 #ticket table {
-  font-family: "Courier New", Courier, monospace;
+  font-family: 'Courier New', Courier, monospace;
   width: 100%;
   border-collapse: collapse;
 }
 
-#ticket td,
-#ticket th {
+#ticket td, #ticket th {
   padding: 8px;
 }
 
@@ -334,8 +337,7 @@ button:hover {
   background-color: #f7f7f7;
 }
 
-#ticket td,
-#ticket #total {
+#ticket td, #ticket #total {
   text-align: left;
 }
 
